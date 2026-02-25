aa2d89a30efd08f4efa887336a7395b1beff4d3e
Admin <admin@orbingerindia.com>
2026-02-26 01:32:57 +0530

    small fix

--- src/config/awsS3.js
+++ src/config/awsS3.js
@@ -3,7 +3,7 @@ import { S3Client } from "@aws-sdk/client-s3"
 export const s3 = new S3Client({
     region: process.env.AWS_RE,
     credentials: {
-        accessKeyId: process.env.AWS_ACC,
+        accessKeyId: process.env.AWS_ACC_KEY,
         secretAccessKey: process.env.AWS_S_KEY
     }
 })
